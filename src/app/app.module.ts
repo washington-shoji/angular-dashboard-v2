46908c84cb7503e40232fce266c5646ec9a53309
washington-shoji <shoji.washington@gmail.com>
2022-10-03 18:28:13 +1100

    feat(component): added starter components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatIconModule } from '@angular/material/icon';
+import { BodyComponent } from './components/body/body.component';
+import { SidenavbarComponent } from './components/sidenavbar/sidenavbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 const materialModules: MatIconModule[] = [MatIconModule];
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BodyComponent, SidenavbarComponent, DashboardComponent, StatisticsComponent, SettingsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
