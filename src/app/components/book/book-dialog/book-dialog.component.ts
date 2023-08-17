66c6b3ebba4bd8dc10327ae0648af4725cd8f2b2
washington-shoji <washington.shoji@automic.com.au>
2023-08-17 21:59:55 +1000

    - added book-dialog

---
+++ src/app/components/book/book-dialog/book-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-dialog',
+  templateUrl: './book-dialog.component.html',
+  styleUrls: ['./book-dialog.component.scss']
+})
+export class BookDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
